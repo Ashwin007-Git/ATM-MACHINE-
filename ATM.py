aca97b82270286ba0c88c2e875d3f96174d3f866
Ashwin007-Git <tjashwin07@gmail.com>
2026-08-28 10:28:43 +0000

    added

--- ATM.py
+++ ATM.py
@@ -1,5 +1,17 @@
 print("WELCOME TO HDFC BANK")
 print("INSERT YOUR ATM CARD")
 pin=1234
-if pin==1234
-print("Choose any option")
+balance=50000
+if pin==1234:
+    print("1.WITHDRAW")
+    print("2.BALANCE ENQUIRY")
+    print("3.DEPOSIT")
+    print("4.CHANGE PIN")
+    print("5.EXIT")
+    option=int(input("Choose any option"))
+    if option==1:
+        withdraw=int(input("Enter the withdraw Amount"))
+        if withdraw<=balance:
+            balance=balance-withdraw
+            print("Amount of ",withdraw ,"withdrawed")
+    
